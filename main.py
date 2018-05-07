4063acf17db8abadea774d0a82ab58adede78f6f
zymantas <z.maumevicius@gmail.com>
2018-05-07 21:20:43 +0300

    Added one more layer and MASE error calculation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from numpy import exp, array, random, dot, mean, abs, tanh
+from numpy import array, random, dot, mean, abs, tanh
 
 
 # MASE error
@@ -12,13 +12,9 @@ class NeuralNetwork:
         random.seed(1)
         self.learning_rate = learning_rate
         self.sympatic_weights = {}
-        self.sympatic_weights[0] = (2 * random.random((4, 4)) - 1)
-        self.sympatic_weights[1] = (2 * random.random((4, 1)) - 1)
-
-    def _sigmoid(self, x, derivative=False):
-        if derivative:
-            return x * (1 - x)
-        return 1 / (1 + exp(-x))
+        self.sympatic_weights[0] = (2 * random.random((4, 8)) - 1)
+        self.sympatic_weights[1] = (2 * random.random((8, 4)) - 1)
+        self.sympatic_weights[2] = (2 * random.random((4, 1)) - 1)
 
     def _tanh(self, x, derivative=False):
         if derivative:
@@ -29,11 +25,16 @@ class NeuralNetwork:
         k0 = training_input
         k1 = self._tanh(dot(k0, self.sympatic_weights[0]))
         k2 = self._tanh(dot(k1, self.sympatic_weights[1]))
+        k3 = self._tanh(dot(k2, self.sympatic_weights[2]))
 
-        k2_error = training_output - k2
+        k3_error = training_output - k3
 
         if i % 1000 == 0:
-            print "Error:" + str(mean(abs(k2_error)))
+            print "Error:" + str(mean(abs(k3_error)))
+
+        k3_delta = k3_error * self._tanh(k3, derivative=True)
+
+        k2_error = k3_delta.dot(self.sympatic_weights[2].T)
 
         k2_delta = k2_error * self._tanh(k2, derivative=True)
 
@@ -41,6 +42,7 @@ class NeuralNetwork:
 
         k1_delta = k1_error * self._tanh(k1, derivative=True)
 
+        self.sympatic_weights[2] += self.learning_rate * k2.T.dot(k3_delta)
         self.sympatic_weights[1] += self.learning_rate * k1.T.dot(k2_delta)
         self.sympatic_weights[0] += self.learning_rate * k0.T.dot(k1_delta)
 
@@ -51,8 +53,9 @@ class NeuralNetwork:
         k0 = x
         k1 = self._tanh(dot(k0, self.sympatic_weights[0]))
         k2 = self._tanh(dot(k1, self.sympatic_weights[1]))
+        k3 = self._tanh(dot(k2, self.sympatic_weights[2]))
 
-        return k2
+        return k3
 
 
 def readAndPrepareData():
@@ -65,6 +68,7 @@ def readAndPrepareData():
     for i in xrange(len(data) - 1):
         resolved_data[i] = {"price": data[i + 1], "change": calculateChange(data[i], data[i + 1])}
 
+    resolved_data = {k: v for k, v in resolved_data.iteritems() if v["change"] < 1}
     return resolved_data
 
 
@@ -73,14 +77,14 @@ def calculateChange(last, now):
 
 
 if __name__ == '__main__':
-    neural_network = NeuralNetwork(learning_rate=0.07)
+    neural_network = NeuralNetwork(learning_rate=0.0001)
 
     data = readAndPrepareData()
 
     print "Initial weight values"
     print neural_network.sympatic_weights
 
-    for i in xrange(1000):
+    for i in xrange(10000):
         for j in xrange(len(data) - 55):
             training_set_inputs = array([
                 [data[j]["change"]],
@@ -91,33 +95,24 @@ if __name__ == '__main__':
             training_set_outputs = array([[data[j + 4]["change"]]])
             neural_network.train(training_set_inputs, training_set_outputs, i * j)
 
-    # count = 0
-    # for j in xrange(len(data) - 55, len(data) - 5):
-    #     test_set_intput = array([
-    #         [data[j]["change"]]
-    #         , [data[j + 1]["change"]]
-    #         , [data[j + 2]["change"]]
-    #         , [data[j + 3]["change"]]]).T
-    #
-    #     test_set_outputs = array([[data[j + 4]["change"]]]).T
-    #
-    #     prediction = neural_network.predict(training_set_inputs)
-    #     if prediction == test_set_outputs:
-    #         count = count + 1
-    #
-    # print "Teisingai prognozuotu kekis"
-    # print count
+    mases = []
+    mae_predictions = []
+    mae_dummys = []
+    for j in xrange(len(data) - 55, len(data) - 5):
+        test_set_intputs = array([
+            [data[j]["change"]]
+            , [data[j + 1]["change"]]
+            , [data[j + 2]["change"]]
+            , [data[j + 3]["change"]]]).T
 
-    print "Updated weight values"
-    print neural_network.sympatic_weights
+        test_set_outputs = array([[data[j + 4]["change"]]])
 
-    print "Predicted value"
-    print neural_network.predict(array([
-        [data[0]["change"]]
-        , [data[1]["change"]]
-        , [data[2]["change"]]
-        , [data[3]["change"]]
-    ]).T)
+        prediction = neural_network.predict(test_set_intputs)
 
-    print "True value"
-    print array([[data[4]["change"]]])
+        mae_predictions.append(mean(abs(test_set_outputs - prediction)))
+        mae_dummys.append(mean(abs(test_set_outputs - array([[data[j + 3]["change"]]]))))
+
+    mase = sum(mae_predictions) / sum(mae_dummys)
+
+    print "avg MASE:"
+    print mase
