19c3be8e420b0e4b07346a45a0e547bd924e7b34
zymantas <z.maumevicius@gmail.com>
2018-05-11 21:39:19 +0300

    Refactor add BIAS

--- neuralNet.py
+++ neuralNet.py
@@ -1,18 +1,17 @@
-from numpy import array, random, dot, mean, abs, tanh, floor
+from numpy import array, random, dot, mean, abs, tanh, ones
 
 
 class Net():
-    def __init__(self, learning_rate=0.1, inputs_count=4):
+    def __init__(self, learning_rate=0.1, inputs_count=4, layer_count=3):
         random.seed(1)
-        self.multiplier = 2
         self.learning_rate = learning_rate
-        n = inputs_count * self.multiplier
-        if n % 2 != 1:
-            n = n + 1
-        self.weights = [(2 * random.random((inputs_count, n)) - 1)]
-        while n != 1:
-            self.weights.append((2 * random.random((int(n), int(n / 2))) - 1))
-            n = int(n / 2)
+        self.weights = [(2 * random.random((inputs_count, 5)) - 1)]
+        self.biases = [1, 1, 1]
+
+        while len(self.weights) != layer_count - 1:
+            self.weights.append((2 * random.random((5, 13)) - 1))
+
+        self.weights.append((2 * random.random((13, 1)) - 1))
 
     def _tanh(self, x):
         return tanh(x)
@@ -22,10 +21,8 @@ class Net():
 
     def _feed_forward(self, inputs):
         results = [inputs]
-        for i in xrange(len(self.weights) - 1):
-            results.append(self._tanh(dot(results[-1], self.weights[i])))
-
-        results.append(dot(results[-1], self.weights[-1]))
+        for i in xrange(len(self.weights)):
+            results.append(self._tanh(dot(results[-1], self.weights[i]) + self.biases[i]))
 
         return results
 
@@ -42,6 +39,9 @@ class Net():
             layer_number = idx * -1
             self.weights[idx] += self.learning_rate * results[idx].T.dot(deltas[layer_number])
 
+        for idx in xrange(len(self.biases)):
+            self.biases[idx] += self.learning_rate * 1 * deltas[-idx]
+
     def train(self, training_input_set, training_output_set, epochs=1000):
         for i in xrange(epochs):
             for input_idx in xrange(len(training_input_set)):
@@ -51,8 +51,12 @@ class Net():
     def predict(self, inputs):
         return self._feed_forward(inputs)[-1]
 
-    def print_weights(self):
+    def print_params(self):
+        print "Weights:"
         print self.weights
+
+        print "Biases:"
+        print self.biases
 
 
 def readAndPrepareData():
@@ -79,13 +83,15 @@ if __name__ == '__main__':
     epochs = 1000
     training_set_size = 70
     test_set_size = 30
+    layer_count = 3
 
-    neural_network = Net(learning_rate=lr, inputs_count=input_count)
+    neural_network = Net(learning_rate=lr, inputs_count=input_count, layer_count=layer_count)
+
+    neural_network.print_params()
 
     data = readAndPrepareData()
 
-    print "Initial weight values"
-    print neural_network.weights
+    print "Training..."
 
     training_set_inputs = []
     training_set_outputs = []
@@ -94,14 +100,14 @@ if __name__ == '__main__':
     for j in xrange((len(data) / 100 * training_set_size) - 4):
         training_set_inputs.append(
             array([
-                array([data[j]["change"]]).T,
-                array([data[j + 1]["change"]]).T,
-                array([data[j + 2]["change"]]).T,
-                array([data[j + 2]["change"]]).T,
-                # [data[j]["change"], j % 12],
-                # [data[j + 1]["change"], j % 12],
-                # [data[j + 2]["change"], j % 12],
-                # [data[j + 3]["change"], j % 12]
+                # array([data[j]["change"]]).T,
+                # array([data[j + 1]["change"]]).T,
+                # array([data[j + 2]["change"]]).T,
+                # array([data[j + 3]["change"]]).T,
+                array([data[j]["change"], j % 12]).T,
+                array([data[j + 1]["change"], j % 12]).T,
+                array([data[j + 2]["change"], j % 12]).T,
+                array([data[j + 3]["change"], j % 12]).T
             ]).T
         )
 
@@ -109,25 +115,27 @@ if __name__ == '__main__':
 
     neural_network.train(training_set_inputs, training_set_outputs, epochs)
 
+    print "Testing..."
+
     mases = []
     mae_predictions = []
     mae_dummys = []
 
     for j in xrange(len(data) / 100 * training_set_size, len(data) - 4):
         test_set_inputs = array([
-            array([data[j]["change"]]).T,
-            array([data[j+1]["change"]]).T,
-            array([data[j+2]["change"]]).T,
-            array([data[j+2]["change"]]).T,
-            # [data[j + 1]["change"], j % 12],
-            # [data[j + 2]["change"], j % 12],
-            # [data[j + 3]["change"], j % 12]
+            # array([data[j]["change"]]).T,
+            # array([data[j + 1]["change"]]).T,
+            # array([data[j + 2]["change"]]).T,
+            # array([data[j + 3]["change"]]).T,
+            array([data[j]["change"], j % 12]).T,
+            array([data[j + 1]["change"], j % 12]).T,
+            array([data[j + 2]["change"], j % 12]).T,
+            array([data[j + 3]["change"], j % 12]).T
         ]).T
 
         test_set_outputs = array([[data[j + 4]["change"]]])
 
         prediction = neural_network.predict(test_set_inputs)
-        print prediction
         mae_predictions.append(mean(abs(test_set_outputs - prediction)))
         mae_dummys.append(mean(abs(test_set_outputs - array([[data[j + 3]["change"]]]))))
 
@@ -135,3 +143,5 @@ if __name__ == '__main__':
 
     print "avg MASE:"
     print mase
+
+    neural_network.print_params()
